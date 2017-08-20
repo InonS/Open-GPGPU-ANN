abd5847718607d3dc596737ff6d54e4b4dc352ab
Inon Sharony <InonShar@TAU.ac.IL>
2017-08-21 02:46:56 +0300

    Changed cost reporting. Added text summary.

--- sentiment_nn_large_data.py
+++ sentiment_nn_large_data.py
@@ -15,8 +15,8 @@ from os import rename
 from os.path import join as path_join, sep
 
 from numpy import array
-from tensorflow import Session, Variable, add, argmax, cast, expand_dims, float32, global_variables_initializer, \
-    matmul, placeholder, random_normal, reduce_mean
+from tensorflow import Session, Variable, add, argmax, cast, constant, expand_dims, float32, \
+    global_variables_initializer, matmul, name_scope, placeholder, random_normal, reduce_mean, string, uint8
 from tensorflow.contrib.learn.python.learn.utils.saved_model_export_utils import get_timestamped_export_dir
 from tensorflow.python.ops.nn_ops import relu, softmax_cross_entropy_with_logits
 from tensorflow.python.saved_model.builder import SavedModelBuilder
@@ -24,6 +24,7 @@ from tensorflow.python.saved_model.loader import load
 from tensorflow.python.saved_model.loader_impl import maybe_saved_model_directory
 from tensorflow.python.saved_model.tag_constants import SERVING
 from tensorflow.python.summary.summary import histogram, merge_all, scalar
+from tensorflow.python.summary.text_summary import text_summary
 from tensorflow.python.summary.writer.writer import FileWriter
 from tensorflow.python.training.adam import AdamOptimizer
 from tensorflow.python.training.tensorboard_logging import DEBUG, debug, info, set_summary_writer, set_verbosity
@@ -105,29 +106,35 @@ def define_operations(data, hidden_layers, output_layer):
     layers = []
     prev_layer = data
     histogram("input_layer", data)
-    histogram("preactivation_layer_%d", data)
-    histogram("activation_layer_%d", data)
     for hidden_layer_index in range(N_HIDDEN_LAYERS):
-        current_layer = hidden_layers[hidden_layer_index]
-        preactivation = sum_(prev_layer, current_layer['weights'], current_layer['biases'])
-        hidden_layer = activate_(preactivation)
-        layers.append(hidden_layer)
-        prev_layer = layers[hidden_layer_index]
-    output = sum_(prev_layer, output_layer['weights'], output_layer['biases'])
+        with name_scope("layer_%d" % hidden_layer_index):
+            current_layer = hidden_layers[hidden_layer_index]
+            preactivation = sum_(prev_layer, current_layer['weights'], current_layer['biases'])
+            histogram("preactivation_layer_%d" % hidden_layer_index, preactivation)
+            hidden_layer = activate_(preactivation)
+            histogram("activation_layer_%d" % hidden_layer_index, hidden_layer)
+            layers.append(hidden_layer)
+            prev_layer = layers[hidden_layer_index]
+    with name_scope("output_layer"):
+        output = sum_(prev_layer, output_layer['weights'], output_layer['biases'])
     return output
 
 
 def define_tensors():
     hidden_layers = define_hidden_layer_tensors()
-    output_layer = define_output_layer_tensors()
+    with name_scope("output_layer"):
+        output_layer = define_output_layer_tensors()
     return hidden_layers, output_layer
 
 
 def define_output_layer_tensors():
     output_weights_shape = [N_HIDDEN_LAYER_NODES[-1], N_OUTPUT_LAYER_NODES]
     output_biases_shape = [N_OUTPUT_LAYER_NODES]
-    output_layer = {'weights': Variable(initial_value=random_normal(output_weights_shape)),
-                    'biases': Variable(initial_value=random_normal(output_biases_shape))}
+    output_layer = {
+        'weights': Variable(initial_value=random_normal(output_weights_shape), name="output_weights", dtype=float32,
+                            expected_shape=output_weights_shape),  # collections=[GraphKeys.WEIGHTS],
+        'biases': Variable(initial_value=random_normal(output_biases_shape), name="output_biases", dtype=float32,
+                           expected_shape=output_biases_shape)}  # collections=[GraphKeys.BIASES],
     histogram("output_layer_weights", output_layer['weights'])
     histogram("output_layer_biases", output_layer['biases'])
     return output_layer
@@ -137,33 +144,51 @@ def define_hidden_layer_tensors():
     hidden_layers = []
     n_prev_layer_nodes = N_INPUT_LAYER_NODES
     for hidden_layer_index in range(N_HIDDEN_LAYERS):
-        n_current_layer_nodes = N_HIDDEN_LAYER_NODES[hidden_layer_index]
-        weights_shape = [n_prev_layer_nodes, n_current_layer_nodes]
-        biases_shape = [n_current_layer_nodes]
-        hidden_layer = {'weights': Variable(initial_value=random_normal(weights_shape)),
-                        'biases': Variable(initial_value=random_normal(biases_shape))}
-        hidden_layers.append(hidden_layer)
-        histogram("layer_%d_weights" % hidden_layer_index, hidden_layer['weights'])
+        with name_scope("layer_%d" % hidden_layer_index):
+            n_current_layer_nodes = N_HIDDEN_LAYER_NODES[hidden_layer_index]
+            weights_shape = [n_prev_layer_nodes, n_current_layer_nodes]
+            biases_shape = [n_current_layer_nodes]
+            hidden_layer = {'weights': Variable(initial_value=random_normal(weights_shape),
+                                                name="layer_%d_weights" % hidden_layer_index, dtype=float32,
+                                                expected_shape=weights_shape),  # , collections=[GraphKeys.WEIGHTS]
+                            'biases': Variable(initial_value=random_normal(biases_shape),
+                                               name="layer_%d_biases" % hidden_layer_index, dtype=float32,
+                                               expected_shape=biases_shape)}  # , collections=[GraphKeys.BIASES]
+            hidden_layers.append(hidden_layer)
+            histogram("layer_%d_weights" % hidden_layer_index, hidden_layer['weights'])
         histogram("layer_%d_biases" % hidden_layer_index, hidden_layer['biases'])
         n_prev_layer_nodes = n_current_layer_nodes
+
     return hidden_layers
 
 
 def mean_cross_entropy(labels, logits):
-    return reduce_mean(softmax_cross_entropy_with_logits(labels=labels, logits=logits))
+    xent = softmax_cross_entropy_with_logits(labels=labels, logits=logits, name="xent")
+    return reduce_mean(xent, name="mean_cross_entropy")
 
 
 def run(data, labels, max_lines_=None, max_epochs=None):
     latest_predictions = model(data)
-    cost = mean_cross_entropy(labels, latest_predictions)
-    optimizer = AdamOptimizer(learning_rate=0.01).minimize(cost)
+    optimizer = build_optimizer(labels, latest_predictions)
 
     # TODO Consider MonitoredTrainingSession or SessionManager
     with Session() as sess:
         sess.run(global_variables_initializer())
-        train(sess, cost, optimizer, max_lines_=max_lines_, max_epochs=max_epochs)
-        # load_or_train_model(sess, cost, optimizer, max_lines_=max_lines_, max_epochs=max_epochs)
-        test(latest_predictions)
+
+        with name_scope("training"):
+            train(sess, optimizer, max_lines_=max_lines_, max_epochs=max_epochs)
+            # load_or_train_model(sess, cost, optimizer, max_lines_=max_lines_, max_epochs=max_epochs)
+
+        with name_scope("testing"):
+            test(latest_predictions)
+
+
+def build_optimizer(labels, latest_predictions):
+    with name_scope("cost_func"):
+        cost = mean_cross_entropy(labels, latest_predictions)
+    scalar("cost", cost)
+    with name_scope("opt_algo"):
+        return AdamOptimizer(learning_rate=0.01).minimize(cost)
 
 
 def load_or_train_model(sess, cost, optimizer, max_lines_=MAX_LINES, max_epochs=None):
@@ -175,7 +200,7 @@ def load_or_train_model(sess, cost, optimizer, max_lines_=MAX_LINES, max_epochs=
         meta_graph_def = load(sess, SERVING, export_dir)
         info("meta-graph def = {}.".format(meta_graph_def))
     else:
-        train(sess, cost, optimizer, max_lines_=max_lines_, max_epochs=max_epochs)
+        train(sess, optimizer, max_lines_=max_lines_, max_epochs=max_epochs)
         saved_model_path = save_served_model(sess)
         saved_model_dir = saved_model_path.split(sep)[:-1]
         info("saved_model_dir = %s" % saved_model_dir)
@@ -194,43 +219,36 @@ def save_served_model(sess):
     return builder.save()
 
 
-def train(sess, cost, optimizer, max_lines_=MAX_LINES, max_epochs=None):
+def train(sess, optimizer, max_lines_=MAX_LINES, max_epochs=None):
     """
-    ~ 80 training samples / minute = 4/31 samples / second = 4800 samples / hour = 1.152e5 samples / day
+    ~ 4/3 samples / second = 4800 samples / hour = 1.152e5 samples / day
     1.6e6 training samples = 13 8/9 days for one-shot (max_epochs = 1).
 
-    (4800 samples / hour) * 5 hours / 10 epochs = 2400 samples
+    (4800 samples / hour) * 5 hours / 1 epochs = 24000 samples
     https://www.tensorflow.org/get_started/summaries_and_tensorboard
     """
     summary_writer = FileWriter(path_join(LOGDIR, "train"), graph=sess.graph)
     set_summary_writer(summary_writer)
     set_verbosity(DEBUG)
-
-    # for sample_text in batch_x:
-    #     text_summary("input_text", [lexicon[word] for word in sample_text])
-    epoch = 0
-    scalar('epoch', epoch)
-    epoch_cost = 0
-    scalar('epoch_cost', epoch_cost)
-    batch_cost = 0
-    scalar('batch_cost', batch_cost)
     merged_summaries = merge_all()
+
+    global_step = 0
 
     if max_epochs is None:
         max_epochs = 1
     for epoch in range(max_epochs):
-        epoch_cost = 0
-
         for sample in generate_design_matrix(train_filepath, lexicon, max_lines_=max_lines_):
-            batch_x, batch_y = array(sample[0]), array(sample[1])
-            batch_feed = {x: batch_x, y: batch_y}  # note placeholder keys
-            _, batch_cost, summary = sess.run(
-                [optimizer, cost, merged_summaries], feed_dict=batch_feed)  # optimizer returns None
-            # info("Batch %d completed out of %d batches, cost: %g" % (batch, total_batches, batch_cost))
-            epoch_cost += batch_cost
-            summary_writer.add_summary(summary)
+            sample_x, sample_y = array(sample[0]), array(sample[1])
 
-        info("Epoch %d completed out of %d epochs, cost: %g " % (epoch + 1, max_epochs, epoch_cost))
+            sample_words = [lexicon[int(word_index)] for word_index in sample_x]
+            lexed_sample = ' '.join(sample_words)
+            sample_text_ = constant(lexed_sample, dtype=string, name="sample_text")
+            text_summary("input_text", sample_text_)
+
+            batch_feed = {x: sample_x, y: sample_y}  # note placeholder keys
+            summary, _ = sess.run([merged_summaries, optimizer], feed_dict=batch_feed)
+            summary_writer.add_summary(summary, global_step=global_step)
+            global_step += 1
 
 
 def test(prediction):
@@ -238,10 +256,11 @@ def test(prediction):
     set_summary_writer(summary_writer)
     set_verbosity(DEBUG)
 
-    n_samples = 0
+    n_samples = Variable(initial_value=constant(0, dtype=uint8), name="n_samples", dtype=uint8, expected_shape=[1])
     scalar('test_samples', n_samples)
     sum_accuracy = 0
-    running_accuracy = 0
+    running_accuracy = Variable(initial_value=constant(0, dtype=float32), name="running_accuracy", dtype=float32,
+                                expected_shape=[1])  # collections=[GraphKeys.MOVING_AVERAGE_VARIABLES],
     scalar('running_accuracy', running_accuracy)
     merged_summaries = merge_all()
 
@@ -265,7 +284,7 @@ def test(prediction):
 def main():
     define_model_params()
     define_global_placeholders()
-    run(x, y, max_lines_=int(1e1))  # , max_lines_=int(2.5e3), max_epochs=10)
+    run(x, y, max_lines_=int(2e4))  # , max_lines_=int(2.5e3), max_epochs=10)
 
 
 if __name__ == '__main__':
