90843a1db1026942da8e827de51a93aec597d449
inon <Inon.Sharony@gmail.com>
2018-04-21 01:50:34 +0300

    OpenCV master branch support for FuzedBatchNormalization still not here... Mobilenet v2 needs it

--- opencv/test_ocv.py
+++ opencv/test_ocv.py
@@ -6,22 +6,30 @@
 3. https://github.com/opencv/opencv/tree/master/samples/dnn
 """
 
-from logging import INFO, basicConfig, debug, info, warning
+from logging import DEBUG, basicConfig, debug, info, warning
+from os.path import extsep, join as path_join, pardir
 from sys import stdout
+from time import perf_counter
 from timeit import timeit
 from typing import List, Tuple
 
 from PIL.Image import Image
-from cv2 import TERM_CRITERIA_EPS, TERM_CRITERIA_MAX_ITER, dnn_Net, getTickFrequency, haveOpenVX, ml_ANN_MLP, \
+from cv2 import TERM_CRITERIA_EPS, TERM_CRITERIA_MAX_ITER, dnn_Net, getTickFrequency, haveOpenVX, ml_ANN_MLP, rectangle, \
     setUseOpenVX, setUseOptimized, useOpenVX, useOptimized
 from cv2.dnn import DNN_BACKEND_HALIDE, DNN_TARGET_OPENCL, blobFromImage, readNetFromTensorflow
 from cv2.ipp import getIppVersion, useIPP, useIPP_NE
 from cv2.ml import ANN_MLP_BACKPROP, ANN_MLP_SIGMOID_SYM, ANN_MLP_create, ROW_SAMPLE, TrainData_create
 from cv2.ocl import finish, haveAmdBlas, haveAmdFft, haveOpenCL, setUseOpenCL, useOpenCL
-from numpy import argmax, float32, int32, ndarray, ones, reshape, sqrt, uint16, uint64, uint8, zeros
+from numpy import argmax, float32, int32, ndarray, ones, reshape, sort, uint16, uint64, uint8, zeros
 from pandas import unique
 from sklearn.datasets import load_digits
-from sklearn.neural_network._base import log_loss
+from sklearn.metrics import log_loss
+from tensorflow import GraphDef, Session
+from tensorflow.python.framework.graph_io import write_graph
+from tensorflow.python.platform.gfile import FastGFile
+from tensorflow.python.tools.freeze_graph import freeze_graph
+from tensorflow.python.tools.optimize_for_inference_lib import optimize_for_inference
+from tensorflow.python.training.saver import Saver
 from tqdm import tqdm, trange
 
 
@@ -34,11 +42,18 @@ def main():
 
     x_train, y_train = get_data()
     x_train = x_train.astype(float32)
-    train_mlp(x_train, y_train.tolist())  # TODO readNetFromTensorflow()
+    mlp: ml_ANN_MLP = train_mlp(x_train, list(y_train.tolist()))
+    time_inference(mlp, x_train)
+
+    model_name = "mobilenet_v2_1.0_96"
+    logdir = path_join(pardir, "data", model_name)
+    frozen_graph = extsep.join(('_'.join((model_name, "frozen")), "pb"))
+    net = read_net_from_tf(model_name, logdir, frozen_graph)
 
     sample = x_train[0]
-    height = sqrt(sample.shape[0])
-    dnn_predict(sample, height, height, uint8(len(unique(y_train))))
+    width = uint16(8)
+    height = uint16(sample.shape[0] / width)
+    dnn_predict(net, (width, height), sample, sort(unique(y_train)))
 
 
 def ocl_setup():
@@ -121,6 +136,49 @@ def train_mlp(training_data: ndarray, index: List[uint8]):
     else:
         warning("training failed!")
 
+    return mlp
+
+
+def poker(index: List[uint8], output_dim, training_data):
+    """
+    one-hot encoding
+    """
+    train_classes = zeros((len(training_data), output_dim),
+                          dtype=float32)  # UMat(training_data.rows, output_neurons, CV_32FC1).get()
+    for i in trange(len(train_classes), desc=None, file=stdout, mininterval=2, unit='sample'):
+        train_classes[i, index[i]] = 1
+        # debug("Row of train_class: {}".format(train_classes[i]))
+        # debug("Row of train_data: {}".format(training_data[i]))
+    return train_classes
+
+
+def koker(mlp: ml_ANN_MLP, train_classes, training_data):
+    """
+    train
+    """
+    assert training_data.dtype == float32 or training_data.dtype == int32  # CV_32S (type 4) or CV_32F (type 5)
+    assert train_classes.dtype == float32
+
+    td = TrainData_create(training_data, ROW_SAMPLE, train_classes)
+    t = timeit("mlp.train(td)", number=1, globals={"mlp": mlp, "td": td})
+    info("Training Done (took %f seconds, %f ms per sample)" % (t, 1e3 * t / len(training_data)))
+
+
+def predict_ones_vector(mlp: ml_ANN_MLP, train_classes, training_data):
+    assert training_data.dtype == int32 or training_data.dtype == float32
+    assert training_data.shape[1] == mlp.getLayerSizes()[0]
+
+    result = zeros(train_classes.shape[1], dtype=float32)
+    assert result.dtype == int32 or result.dtype == float32
+
+    sample = ones((1, training_data.shape[1]), training_data.dtype)
+    assert sample.dtype == int32 or sample.dtype == float32
+    assert sample.shape[1] == mlp.getLayerSizes()[0]
+
+    predicted, result = mlp.predict(sample)
+    info("One-vector prediction {}".format("failed!" if not predicted else ": {}".format(result)))
+    return result
+
 
 def predict_validation(mlp: ml_ANN_MLP, training_data, train_classes):
     predicted = zeros(train_classes.shape[1], dtype=float32)
@@ -140,78 +198,121 @@ def predict_validation(mlp: ml_ANN_MLP, training_data, train_classes):
         ll += log_loss(reshape(expected, (1, len(expected))), result)
         is_correct = predicted == argmax(expected)  # and allclose(predicted, expected, rtol=5e-2, atol=5e-2)
         n_correct += 1 if is_correct else 0
-        debug("Predict training data ({}): {}".format(is_correct, predicted))
+        # debug("Predict training data ({}): {}".format(is_correct, predicted))
     info("accuracy = %f, log loss per sample = %f" % (
         n_correct / len(training_data), ll / len(training_data)))  # https://en.wikipedia.org/wiki/Confusion_matrix
 
 
-def predict_ones_vector(mlp: ml_ANN_MLP, train_classes, training_data):
-    assert training_data.dtype == int32 or training_data.dtype == float32
-    assert training_data.shape[1] == mlp.getLayerSizes()[0]
+def time_inference(mlp: ml_ANN_MLP, samples, use_timeit=False):
+    if use_timeit:
+        results = None
+        t = timeit("_, results = mlp.predict(samples)", number=1,
+                   globals={"mlp": mlp, "samples": samples, "results": results})
+    else:
+        start = perf_counter()
+        _, results = mlp.predict(samples)
+        t = perf_counter() - start
 
-    result = zeros(train_classes.shape[1], dtype=float32)
-    assert result.dtype == int32 or result.dtype == float32
-
-    sample = ones((1, training_data.shape[1]), training_data.dtype)
-    assert sample.dtype == int32 or sample.dtype == float32
-    assert sample.shape[1] == mlp.getLayerSizes()[0]
-
-    predicted, result = mlp.predict(sample)
-    info("One-vector prediction {}".format("failed!" if not predicted else ": {}".format(result)))
-    return result
+    info("Inference Done (took %f seconds, %f ms per sample)" % (t, 1e3 * t / len(samples)))
 
 
-def koker(mlp: ml_ANN_MLP, train_classes, training_data):
-    assert training_data.dtype == float32 or training_data.dtype == int32  # CV_32S (type 4) or CV_32F (type 5)
-    assert train_classes.dtype == float32
-
-    td = TrainData_create(training_data, ROW_SAMPLE, train_classes)
-    t = timeit("mlp.train(td)", number=1, globals={"mlp": mlp, "td": td})
-    print("Training Done (took %f seconds, %f ms per sample)" % (t, 1e3 * t / len(training_data)))
-
-
-def poker(index: List[uint8], output_dim, training_data):
+def tf_freeze_graph(sess: Session, saver: Saver, model_name: str, logdir: str,
+                    model_suffix: str = "pb", conf_suffix: str = "pbtxt", checkpoint_suffix: str = "ckpt"):
     """
-    one-hot encoding
+    python freeze_graph.py --output_graph =./pbs/frozenGraph.pb --output_node_names=genderOut,ageOut --input_binary=true
     """
-    train_classes = zeros((len(training_data), output_dim),
-                          dtype=float32)  # UMat(training_data.rows, output_neurons, CV_32FC1).get()
-    for i in trange(len(train_classes), desc=None, file=stdout, mininterval=2, unit='sample'):
-        train_classes[i, index[i]] = 1
-        debug("Row of train_class: {}".format(train_classes[i]))
-        debug("Row of train_data: {}".format(training_data[i]))
-    return train_classes
+    input_conf = write_graph(sess.graph_def, logdir, extsep.join('_'.join((model_name, "graphDef")), conf_suffix))
+
+    input_binary = True
+    input_graph = write_graph(sess.graph_def, logdir, extsep.join('_'.join((model_name, "graphDef")), model_suffix),
+                              as_text=False)
+
+    input_checkpoint = saver.save(sess, path_join(logdir, extsep.join(model_name, checkpoint_suffix)))
+
+    output_node_names = ""
+
+    output_graph = path_join(logdir, extsep.join('_'.join((model_name, "frozenGraphDef")), model_suffix))
+    freeze_graph(input_graph, "", input_binary, input_checkpoint, output_node_names, "save/restore_all", "save/Const:0",
+                 output_graph, True, "")
+    return output_graph
 
 
-def dnn_predict(frame: Image, width: uint16, height: uint16, classes: uint8):
-    """
-    TODO https://github.com/tensorflow/models/tree/master/research
-    e.g. object_detection, TF-slim
-    """
-    net: dnn_Net = readNetFromTensorflow()
+def optimize_frozen_graph(logdir: str, frozen_graph: str):
+    with FastGFile(path_join(logdir, frozen_graph), mode='rb') as frozen_graph_file:
+        frozen_graph_def = GraphDef()
+        frozen_graph_def.ParseFromString(frozen_graph_file.read())
+        optimized_frozen_graph_def = optimize_for_inference(frozen_graph_def, ["Reshape"], ["softmax"], [])
+        optimized_frozen_graph_as_bytes = optimized_frozen_graph_def.SerializeToString()
+
+    optimized_frozen_graph = frozen_graph.replace("_frozen", "_frozen_optimized")
+    optimized_frozen_graph_path = path_join(logdir, optimized_frozen_graph)
+    with open(optimized_frozen_graph_path, mode='wb') as optimized_frozen_graph_file:
+        optimized_frozen_graph_file.write(optimized_frozen_graph_as_bytes)
+
+    return optimized_frozen_graph_path
+
+
+def read_net_from_tf(model_name: str, logdir: str, frozen_graph: str = None):
+    if frozen_graph is None:
+        with Session as sess:
+            frozen_graph = tf_freeze_graph(sess, Saver(), model_name, logdir)
+
+    debug(frozen_graph)
+
+    graph_path = path_join(logdir, frozen_graph)  # optimize_frozen_graph(logdir, frozen_graph)
+
+    net: dnn_Net = readNetFromTensorflow(graph_path)
     net.setPreferableBackend(DNN_BACKEND_HALIDE)
     net.setPreferableTarget(DNN_TARGET_OPENCL)
+    return net
 
-    blob = blobFromImage(frame, size=(width, height))
+
+def dnn_predict(net: dnn_Net, input_shape, frame: Image, classes):
+    """
+    https://github.com/opencv/opencv/tree/master/samples/dnn
+
+    https://github.com/tensorflow/models/tree/master/research
+    e.g. object_detection (https://github.com/opencv/opencv/wiki/TensorFlow-Object-Detection-API), TF-slim
+
+    https://github.com/opencv/opencv_extra/blob/master/testdata/dnn/download_models.py
+    """
+    blob = blobFromImage(frame, size=input_shape)
 
     # Run the model
     net.setInput(blob)
     out = net.forward()
 
-    # Class with the highest score
-    out = out.flatten()
-    class_id = argmax(out)
-    confidence = out[class_id]
+    # Single object classification: Class with the highest score
+    flat_out = out.flatten()
+    class_id = argmax(flat_out)
+    confidence = flat_out[class_id]
+
+    # Predicted class
+    info('%s: %.4f' % (classes[class_id] if classes else 'Class #%d' % class_id, confidence))
+
+    # Multiple object detection:
+    bbox_color = (0, 255, 0)
+    threshold = 0.5  # 0.3
+    for detection in out[0, 0, :, :]:
+        score = float(detection[2])
+        if score > threshold:
+            left = detection[3] * frame.width
+            top = detection[4] * frame.height
+
+            right = detection[5] * frame.width
+            bottom = detection[6] * frame.height
+
+            tl = (uint16(left), uint16(top))
+            br = (uint16(right), uint16(bottom))
+
+            rectangle(frame, tl, br, bbox_color)
 
     # Efficiency information
     t, _ = net.getPerfProfile()
     info('Inference time: %.2f ms' % (t * 1000.0 / getTickFrequency()))
 
-    # Predicted class
-    info('%s: %.4f' % (classes[class_id] if classes else 'Class #%d' % class_id, confidence))
-
 
 if __name__ == '__main__':
-    basicConfig(stream=stdout, level=INFO)
+    basicConfig(stream=stdout, level=DEBUG)
     main()
     finish()
